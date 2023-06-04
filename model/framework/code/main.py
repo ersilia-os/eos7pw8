49afd77309fced3cbeb864086dfdf9f83f1af078
samuelmayna <samuelmayna@gmail.com>
2023-06-04 16:14:22 +0300

    Changed the model from predict.py to main.py

---
+++ model/framework/code/main.py
@@ -0,0 +1,50 @@
+# imports
+import os
+import sys
+import csv
+
+from rdkit import Chem
+from syba.syba import SybaClassifier
+
+syba = SybaClassifier()
+syba.fitDefaultScore()
+
+# parse arguments
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+
+# current file directory
+root = os.path.dirname(os.path.abspath(__file__))
+
+# read SMILES from .csv file, assuming one column with header
+with open(input_file, "r") as f:
+    reader = csv.reader(f)
+    next(reader)  # skip header
+    smiles_list = [r[0] for r in reader]
+        
+        
+def my_model(smiles):
+    preds = []
+    for smi in smiles:
+        mol = Chem.MolFromSmiles(smi)
+        if mol is None:
+            preds.append(None)
+        else:
+            preds.append(syba.predict(mol=mol))
+    return preds
+
+# run model
+outputs = my_model(smiles_list)
+
+#check input and output have the same lenght
+input_len = len(smiles_list)
+output_len = len(outputs)
+assert input_len == output_len
+
+# write output in a .csv file
+with open(output_file, "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(["value"])  # header
+    for o in outputs:
+        writer.writerow([o])
